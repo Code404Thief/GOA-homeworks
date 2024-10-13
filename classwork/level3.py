220fd4304feefa6dbcd2c358ebcb647527435d7d
Code404Thief <usernamename9878@gmail.com>
2024-10-13 14:47:35 +0400

    first commit

---
+++ classwork/level3.py
@@ -0,0 +1,10 @@
+#level 3: შექმენი ცვლადები რამოდენიმე საკითხზე, სახელი, გვარი, ასაკი, ქვეყანა, საცხოვრებელი ადგილი, საყვარელი მანქანა, საყვარელი ჰობი, საყვარელი ფერი, შემდეგ ამ შექმნილი ცვლადებით ააგეთ წინადადება.
+name = "sandro"
+lastname = "rukhadze"
+age = 13
+country = "Georgia"
+address = "near caucasus university"
+favourite_car = "ferrari"
+favourite_hobby = "chess"
+favourite_color = "black"
+print("hello my name is " + name + "," + lastname + " " + name + " I am " + str(age) + " years old, I live in " + country + " " + address + " my favourite car is " + favourite_car + " favourite hobby " + favourite_hobby + " my favourite color is similiar to lot of others favourite color which is " + favourite_color )
