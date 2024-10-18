1374e95b267bc85663fc131bb7790841b3646d4a
Code404Thief <usernamename9878@gmail.com>
2024-10-18 18:27:56 +0400

    third commit

---
+++ level 3/level 3.py
@@ -0,0 +1,14 @@
+# davaleba n1
+num1 = 4
+num2 = 15
+num3 = 13
+print(num1 + num2 + num3)
+#davaleba n2
+x = 6
+y = 12
+print(x * y)
+#davaleba n3
+length = 20
+width = 10
+print(2*length + 2*width)
+
